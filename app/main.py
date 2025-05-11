4fd091c4bfcca1e43be093c4ceaffb44d0f2df86
Leegiyeon <rldus3512@naver.com>
2025-05-12 02:26:29 +0900

    feat: router 추가

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.api import recommend
+from app.api import admin_ingest
 
 app = FastAPI()
 app.include_router(recommend.router)
+app.include_router(admin_ingest.router)
 
 @app.get("/")
 async def root():
